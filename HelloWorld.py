ebda2628e5c3cec9cf2bc5ba2a2aed912bc0a211
MohammadHossein Misaghpour <misagh1999@gmail.com>
2018-08-21 11:02:47 +0430

    HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+#--- Hello World Program
+
+n=raw_input("please enter your name: ")
+print "hello %s" % n
+print ("how are you " + n)
+print ("my name is" + " mohammadhossein")
+age=raw_input('How old are you?: ')
+print "your age is: " + age
