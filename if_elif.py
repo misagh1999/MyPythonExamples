72a42a39b8d25dc4a88017da4d15a49585fcdf8e
MohammadHossein Misaghpour <misagh1999@gmail.com>
2018-08-21 11:51:57 +0430

    edit if_elif.py

--- if_elif.py
+++ if_elif.py
@@ -1,12 +1,10 @@
 m=raw_input('enter your number: ')
 m=int(m)
-if m<50:
+if m>50:
     if(m<=20):
         print "your number is between 10 & 20"
     else:
         print "%d is bigger than 20" % m
-elif (m>100):
-    print("your number is bigger than 100")
 else:
     print "your number is smaller than 10"
     if(m<0):
