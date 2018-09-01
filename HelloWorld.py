cba7fb60772e17301b22079dcdb60f84fa2a11e8
MohammadHossein Misaghpour <misagh1999@gmail.com>
2018-09-01 11:22:17 +0430

    I am a developer

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,5 @@
 #--- Hello World Program
 
 n=raw_input("please enter your name: ")
-print "hello %s" % n
-print ("how are you " + n)
-print ("my name is" + " mohammadhossein")
-age=raw_input('How old are you?: ')
-print "your age is: " + age
+
+print "I am a develoer"
